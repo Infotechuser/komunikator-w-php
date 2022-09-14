387d14ed8e0d6e3e414880b057b913a001cbfe82
Edward Krzywiec <edward.krzywiec@infotech.edu.pl>
2022-09-14 12:27:27 +0200

    pierwszy commit

---
+++ folder/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="styles/mail.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div class="header_container">
+            <?php define("logo", "logo")?>
+            <h1><img src="C:\Users\edeks\OneDrive\Obrazy\chat.png" alt="logo"></h1>
+            <nav>
+                <ul>
+                    <li>home</li>
+                    <li>o nas</li>
+                    <li>kontakt</li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <section>
+        <p>Lorem, ipsum.</p>
+        <p>Quidem, deleniti?</p>
+        <p>Fuga, ut?</p>
+        <p>Minus, nulla?</p>
+        <p>Accusamus, vitae?</p>
+        <p>Explicabo, laudantium.</p>
+        <p>Molestiae, et?</p>
+        <p>Quaerat, ad!</p>
+    </section>
+    <footer>
+        @ 2022 Edward Krzywiec
+    </footer>
+</body>
+</html>
